756df532a2d35e85f76802422c924a108085f053
Paulo Alexandre <paulequilibrio@gmail.com>
2020-06-15 06:24:03 -0300

    Add reset and normalize css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 
-import './index.css'
+import './styles/index.css'
 import App from './App'
 
 ReactDOM.render(
